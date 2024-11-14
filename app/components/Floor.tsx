05ee1b7cf524ad933bafa182e91d2e258de20465
adasils <91526030+adasils@users.noreply.github.com>
2024-11-14 19:32:13 +0100

    Update Floor.tsx

--- app/components/Floor.tsx
+++ app/components/Floor.tsx
@@ -2,15 +2,13 @@
 
 import React from "react";
 
-import { MeshProps } from '@react-three/fiber';
-
-const Floor = (props: MeshProps) => {
+function Floor(props) {
   return (
-    <mesh {...props} receiveShadow>
-      <boxBufferGeometry args={[20, 1, 10]} />
-      <meshStandardMaterial color="white" />
+    <mesh {...props} recieveShadow>
+      <boxBufferGeometry args={[20,1,10]} />
+      <meshPhysicalMaterial color='white' />
     </mesh>
   );
-};
+}
 
 export default Floor;
